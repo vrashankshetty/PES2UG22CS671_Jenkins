7a83a72c878333e56b96c1cfa32d1e6e11323fee
Vrashank Shetty <vrashankshetty.s@gmail.com>
2025-03-11 15:54:29 +0530

    Added a print statement ++

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout<< "SRN: PES2UG22CS671 Bye Bye"<<endl;
+  cout<< "SRN: PES2UG22CS671 Bye Bye !!"<<endl;
   cout << "I have successfully built and run this  \n";
   return 0;
 }
