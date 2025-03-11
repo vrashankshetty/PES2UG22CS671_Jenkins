01f28cd271142afb2a1f5e995035d72a5ff3c51d
Vrashank Shetty <vrashankshetty.s@gmail.com>
2025-03-11 16:25:46 +0530

    add jenkinsfile

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout<< "Welcome to pipeline PES2UG22CS671"<<endl;
+  cout<< "Welcome to pipeline PES2UG22CS671.."<<endl;
   cout << "";
   return 0;
 }
